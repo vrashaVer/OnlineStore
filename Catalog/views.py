81c76894ca00712c9df5559f75fedbdbfbb8efb6
vrashaVer <da.programs.vr@gmail.com>
2025-01-25 11:58:06 +0200

    header/footer
    test version of header and footer, without catalog and urls

--- Catalog/views.py
+++ Catalog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+# from models import Category, Collection, ClothingItem, ItemPhoto, ItemColor, Tag, MaterialComposition,FootwearSize, ClothingTopSize,ClothingBottomSize,Like
+from django.contrib.auth.models import User
+from django.views.generic import TemplateView
 
-# Create your views here.
+class TestView(TemplateView):
+    template_name = "test_page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Test Page"
+        return context
+
+
+
+
+
